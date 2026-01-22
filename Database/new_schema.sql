0503904b1f667267858c52bcf0848e580172ad57
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-01-22 13:54:23 +0300

    feat: add stockMovement table

--- Database/new_schema.sql
+++ Database/new_schema.sql
@@ -12,12 +12,24 @@ do $$
         if not exists (select 1 from pg_type where typname = 'unit_type') then
             create type unit_type as enum ('PCS', 'KG', 'L');
         end if;
+        if not exists(select 1 from pg_type where typname = 'movement_type') then
+            create type movement_type as enum ('IN', 'OUT');
+        end if;
     end $$;
 
 create table if not exists DishIngredient (
     id serial primary key,
     dish_id int references dish(id) on delete cascade not null,
-    ingredient_id int references public.ingredient(id) on delete cascade not null,
+    ingredient_id int references ingredient(id) on delete cascade not null,
     quantity decimal(10,2) not null,
     unit unit_type not null
-)
+);
+
+create table if not exists StockMovement (
+    id serial primary key,
+    id_ingredient int references  ingredient(id) on delete cascade not null,
+    quantity numeric(10,2),
+    type movement_type,
+    unit unit_type,
+    creation_datetime timestamp
+);
